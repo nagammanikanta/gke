dd87587974f900d2aa66a40653a031be29df8ade
nagammanikanta <nagammanikanta@gmail.com>
2023-09-29 17:00:10 +0530

    init

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "google_project" "project" {}
 
 
 resource "google_kms_crypto_key_iam_member" "kms-secret-binding" {
-crypto_key_id = "kms-key"
+crypto_key_id = "kms-secret-binding"
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:terraform-${data.google_project.project.number}@data-rainfall-396303.iam.gserviceaccount.com"
 }
@@ -14,20 +14,20 @@ resource "google_secret_manager_secret" "secret-with-automatic-cmek" {
   replication {
     auto {
       customer_managed_encryption {
-        kms_key_name = "kms-key"
+        kms_key_name = "kms-secret-binding"
       }
     }
   }
 
   depends_on = [ google_kms_crypto_key_iam_member.kms-secret-binding ]
 }
-resource "google_kms_key_ring" "my_key_ring" {
+resource "google_kms_key_ring" "my-key-ring" {
  
   name     = "my-key-ring"
   location = "us-central1"
 }
 
-resource "google_kms_crypto_key" "my_crypto_key" {
+resource "google_kms_crypto_key" "my-crypto-key" {
   name     = "my-crypto-key"
   key_ring = google_kms_key_ring.my_key_ring.id
 }
