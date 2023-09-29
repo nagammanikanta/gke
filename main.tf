ca2f24cf3ec29648382bad5835c1a9bc177ef9be
nagammanikanta <nagammanikanta@gmail.com>
2023-09-29 16:57:42 +0530

    init

--- main.tf
+++ main.tf
@@ -21,3 +21,13 @@ resource "google_secret_manager_secret" "secret-with-automatic-cmek" {
 
   depends_on = [ google_kms_crypto_key_iam_member.kms-secret-binding ]
 }
+resource "google_kms_key_ring" "my_key_ring" {
+ 
+  name     = "my-key-ring"
+  location = "us-central1"
+}
+
+resource "google_kms_crypto_key" "my_crypto_key" {
+  name     = "my-crypto-key"
+  key_ring = google_kms_key_ring.my_key_ring.id
+}
