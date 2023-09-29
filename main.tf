a3d6ac44ed643332c09953e9a1eeba3eadbbd9fa
nagammanikanta <nagammanikanta@gmail.com>
2023-09-29 16:49:15 +0530

    init

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "google_project" "project" {}
 
 
 resource "google_kms_crypto_key_iam_member" "kms-secret-binding" {
-  crypto_key_id = "cro-kms-key"
+  crypto_key = "cro-kms-key"
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:terraform-${data.google_project.project.number}@data-rainfall-396303.iam.gserviceaccount.com"
 }
