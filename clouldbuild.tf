73cd938f53e7d493ece28bb70e33c477f27c2d06
nagammanikanta <nagammanikanta@gmail.com>
2023-09-30 10:41:42 +0530

    init

---
+++ clouldbuild.tf
@@ -0,0 +1,15 @@
+resource "google_cloudbuild_trigger" "filename-trigger" {
+  location = "us-central1"
+
+  trigger_template {
+    branch_name = "main"
+    repo_name   = "my-repo"
+  }
+
+  substitutions = {
+    _FOO = "bar"
+    _BAZ = "qux"
+  }
+
+  filename = "test-cloudbuild.yaml"
+}
