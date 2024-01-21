38a2e384cc7bb617407ba32de05d0368f49de006
nagammanikanta <nagammanikanta@gmail.com>
2024-01-21 14:06:32 +0530

    init

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 data "google_compute_instance" "existing_instance" {
   name = "nexus"
-  
+  project ="leafy-summer-405104"
   zone = "us-central1-a"
+  depends_on = [google_compute_instance.nexus]
+  
 }
