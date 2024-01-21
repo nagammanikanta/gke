18299a37d38c6b42390751b45180627b80a8d33f
nagammanikanta <nagammanikanta@gmail.com>
2024-01-21 12:37:58 +0530

    init

--- k8sprovider.tf
+++ k8sprovider.tf
@@ -1,5 +1,16 @@
 terraform {
   required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.12.0"
+    }
+  }
+}
+
+
+
+terraform {
+  required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.25.2"
