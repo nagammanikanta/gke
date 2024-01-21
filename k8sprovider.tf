1dc7b5ba7487a40ed2b5298aea42b55a9da429f1
nagammanikanta <nagammanikanta@gmail.com>
2024-01-21 12:40:02 +0530

    init

--- k8sprovider.tf
+++ k8sprovider.tf
@@ -1,23 +1,13 @@
 terraform {
   required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "5.12.0"
-    }
-  }
-}
-
-
-
-terraform {
-  required_providers {
     kubernetes = {
-      source  = "hashicorp/kubernetes"
+      source = "hashicorp/kubernetes"
       version = "2.25.2"
     }
   }
 }
 
 provider "kubernetes" {
+  # Configuration options
   config_path = "~/.kube/config"
 }
