3d24ef843df0fdb6ead3cc158e470d9b43d5050b
nagammanikanta <nagammanikanta@gmail.com>
2024-01-21 12:18:42 +0530

    init

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "kubernetes_deployment" "example" {
+resource "kubernetes_deployment" "frontend" {
   metadata {
-    name = "terraform-example"
+    name = "frontend"
     labels = {
-      test = "MyExampleApp"
+      test = "frontend"
     }
   }
 
@@ -11,49 +11,48 @@ resource "kubernetes_deployment" "example" {
 
     selector {
       match_labels = {
-        test = "MyExampleApp"
+        test = "webapp"
       }
     }
 
     template {
       metadata {
         labels = {
-          test = "MyExampleApp"
+          test = "webapp"
         }
       }
 
       spec {
         container {
           image = "nginx:1.21.6"
-          name  = "example"
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
+          name  = "sample-webapp"
+          port {
+            container_port = 8080
+            protocol       = "TCP"
           }
         }
       }
     }
   }
 }
+#********* service*****************
+
+resource "kubernetes_service" "svc" {
+    depends_on = [kubernetes_deployment.frontend]
+  metadata {
+    name = "frontend"
+  }
+  spec {
+    selector = {
+      test = "webapp"
+    }
+    port {
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
+  }
+  
+}
+
