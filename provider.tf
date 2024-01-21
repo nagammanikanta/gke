9bea7ccc78af109d01c0e47fed44aba2b69bb1a7
nagammanikanta <nagammanikanta@gmail.com>
2024-01-21 12:32:00 +0530

    init

--- provider.tf
+++ provider.tf
@@ -10,3 +10,17 @@ terraform {
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
+
+provider "kubernetes" {
+  config_paths = [
+    "/path/to/config_a.yaml",
+    "/path/to/config_b.yaml"
+  ]
+}
+provider "kubernetes" {
+  host = "https://cluster_endpoint:port"
+
+  client_certificate     = file("~/.kube/client-cert.pem")
+  client_key             = file("~/.kube/client-key.pem")
+  cluster_ca_certificate = file("~/.kube/cluster-ca-cert.pem")
+}
